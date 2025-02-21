fbd82cf5df77da621073f0f1c415fb54d5913cde
samyog7901 <samyogkhadka247@gmail.com>
2025-02-21 08:54:44 +0545

    finished

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 
 const authRoute = require('./routes/authRoute')
 const organizationRoute = require('./routes/organizationRoute')
+const {Server, Socket} = require('socket.io')
+
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
 
 const cookieParser = require('cookie-parser')
 require("./model/index")
@@ -10,9 +14,21 @@ require("./model/index")
 app.use(cookieParser())//invoked
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use("",authRoute)
-app.use("",organizationRoute)
+// app.use("",authRoute)
+// app.use("",organizationRoute)
 
-app.listen(4000,()=>{
+app.get("/",(req,res)=>{
+    res.render("chat")
+})
+
+const server = app.listen(4000,()=>{
     console.log('server is running on port 4000')
+})
+const io = new Server(server)
+
+io.on("connection",(socket)=>{
+    console.log("new client connected")
+    socket.on("message",(msg)=>{
+        io.emit("broadCastMessage",msg)
+    })
 })
--- routes/organizationRoute.js
+++ routes/organizationRoute.js
@@ -3,9 +3,10 @@ const { isAuthenticated } = require("../middleware/isAuthenticated")
 
 const router = require("express").Router()
 
-router.route("/organization").post(isAuthenticated,createOrganization,createBlogTable)
+// router.route("/organization").post(isAuthenticated,createOrganization,createBlogTable)
 
-router.route("/deleteuser").delete(isAuthenticated,deleteUser)
+// router.route("/deleteuser").delete(isAuthenticated,deleteUser)
+// router.route("/blog").post(isAuthenticated, createBlogTable)
 
 
 
